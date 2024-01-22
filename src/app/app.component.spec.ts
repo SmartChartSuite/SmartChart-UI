35622e76630bb391176258ce3ccef4f433a50c7b
Ellie Shivers <elliemshivers@gmail.com>
2024-01-22 10:03:27 -0500

    Updated to NG17

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('SmartChart-UI');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SmartChart-UI app is running!');
-  });
 });
