9e858552c9e32f7db04ff99c85c996797aba8aef
Ellie Shivers <elliemshivers@gmail.com>
2024-01-25 12:07:27 -0500

    Added login/logout flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
+import {AuthService} from "@auth0/auth0-angular";
+import {ConfigService} from "./services/config/config.service";
+import {skipWhile} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'SmartChart-UI';
+export class AppComponent implements OnInit {
+  title = '';
+  authorized: boolean = false;
+
+  constructor(public auth: AuthService, config: ConfigService) {
+    this.title = config.config.title;
+  }
+
+  ngOnInit(): void {
+  }
+
+
 }
--- src/app/components/content-root/content-root.component.ts
+++ src/app/components/content-root/content-root.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import {StateManagementService} from "../../services/state-management/state-management.service";
+import {AuthService} from "@auth0/auth0-angular";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-content-root',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton
+  ],
   templateUrl: './content-root.component.html',
   styleUrl: './content-root.component.scss'
 })
 export class ContentRootComponent {
 
-  constructor(protected stateManagementService: StateManagementService) {}
+  constructor(
+    protected stateManagementService: StateManagementService,
+    public auth: AuthService) {}
 
   setState(currentComponent: string) {
     this.stateManagementService.setState({"currentComponent": currentComponent});
   }
+
+  protected readonly window = window;
 }
