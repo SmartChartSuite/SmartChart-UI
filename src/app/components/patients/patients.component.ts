f19a32e852d9c333ee00579eede589b49b93a5e6
Plamen Tassev <plamen@gatech.edu>
2024-02-13 12:47:11 -0500

    added patient data

--- src/app/components/patients/patients.component.ts
+++ src/app/components/patients/patients.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RcApiInterfaceService} from "../../services/rc-api-interface/rc-api-interface.service";
 
 @Component({
   selector: 'app-patients',
   templateUrl: './patients.component.html',
   styleUrl: './patients.component.scss'
 })
-export class PatientsComponent {
+export class PatientsComponent implements OnInit{
+  constructor(private rcApiInterfaceService: RcApiInterfaceService){}
 
+  ngOnInit(): void {
+    const result = this.rcApiInterfaceService.searchPatient().subscribe({
+      next: value => {
+        console.log(value)
+      }
+    });
+  }
 }
