c93505088d810ca557ac199e0f7947bbb94f2b81
Plamen Tassev <plamen@gatech.edu>
2024-02-29 10:41:30 -0500

    Added paginator for the results

--- src/app/components/patient-summary-table/patient-summary-table.component.ts
+++ src/app/components/patient-summary-table/patient-summary-table.component.ts
@@ -1,25 +1,29 @@
 import {
+  AfterViewInit,
   Component,
   EventEmitter,
   Input,
   OnChanges,
   Output,
-  SimpleChanges,
+  SimpleChanges, ViewChild,
 } from '@angular/core';
 import {PatientSummary} from "../../models/patient-summary";
 import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-patient-summary-table',
   templateUrl: './patient-summary-table.component.html',
   styleUrl: './patient-summary-table.component.scss'
 })
-export class PatientSummaryTableComponent implements OnChanges {
+export class PatientSummaryTableComponent implements OnChanges, AfterViewInit {
 
   @Input() selectedPatient: PatientSummary | null;
   @Input() patientSummaryData: PatientSummary[] = [];
 
   @Output() patientSelectedEvent: EventEmitter<PatientSummary> = new EventEmitter<PatientSummary>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   displayedColumns: string[] = [ "name", "gender", "birthDate"];
   dataSource: MatTableDataSource<PatientSummary> = new MatTableDataSource<PatientSummary>([]);
@@ -27,10 +31,15 @@ export class PatientSummaryTableComponent implements OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     if(changes['patientSummaryData']?.currentValue){
       this.dataSource.data = this.patientSummaryData;
+      this.dataSource.paginator.firstPage();
     }
   }
   setSelectedPatient(patient: PatientSummary) {
     this.patientSelectedEvent.emit(patient);
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
