5a10c7334b68e1710fb4eff43bd0e2f6af1d3d34
Plamen Tassev <plamen@gatech.edu>
2024-04-04 15:07:47 -0400

    implemented a simple table for the active forms

--- src/app/components/active-forms/active-forms.component.ts
+++ src/app/components/active-forms/active-forms.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {RcApiInterfaceService} from "../../services/rc-api-interface/rc-api-interface.service";
-import {Observable} from "rxjs";
 import {ActiveFormSummary} from "../../models/active-form-summary";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-active-forms',
@@ -10,10 +10,19 @@ import {ActiveFormSummary} from "../../models/active-form-summary";
 })
 export class ActiveFormsComponent implements OnInit {
 
-  batchJobs$: Observable<ActiveFormSummary[]>;
+  displayedColumns: string[] = ["formName", "started", "name", "gender" , 'dob'];
+  dataSource: MatTableDataSource<ActiveFormSummary> = new MatTableDataSource<ActiveFormSummary>([]);
+
   constructor(private rcApiInterfaceService: RcApiInterfaceService) {}
   ngOnInit(): void {
-      this.batchJobs$ = this.rcApiInterfaceService.getBatchJobs()
+    this.rcApiInterfaceService.getBatchJobs().subscribe({
+      next: value => {
+        this.dataSource.data = value;
+      },
+      error: err => {
+        console.error(err);
+      }
+    })
   }
 
 }
