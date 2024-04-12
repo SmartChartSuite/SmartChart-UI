cb0bca3cfb12fd2d781a8852b9be1e9c4f290970
Plamen Tassev <plamen@gatech.edu>
2024-04-12 17:37:12 -0400

    Temp commit in case I am hit by a buss :-)

--- src/app/components/form-viewer/form-viewer.component.ts
+++ src/app/components/form-viewer/form-viewer.component.ts
@@ -1,3 +1,5 @@
+import {FormControl, FormGroup} from "@angular/forms";
+
 export enum QuestionWidgetType{
   CHECKBOX = 'choice',
   INPUT = 'string'
@@ -16,22 +18,32 @@ export class FormViewerComponent implements OnInit {
 
   temp_for_demo: any;
   QuestionWidgetType = QuestionWidgetType;
+
   constructor(private rcApiInterfaceService: RcApiInterfaceService) {}
 
   ngOnInit(): void {
     // TODO: Change this to not be static, once linked to the input should be based on the FormSummary name (NOT title)
     this.rcApiInterfaceService.getJobPackage("SETNETInfantFollowUp").subscribe({
-      next: value => {
-        console.log(value);
-        value['item'] = value['item']?.map((item: any, index: number) => {
+      next: result => {
+        result['item'] = result['item']?.map((item: any) => {return {...item, value: null}});
+        result['item'] = result['item']?.map((item: any, index: number) => {
           return index == 0 ? {...item, selected: true} : {...item, selected: false}
         });
-        this.temp_for_demo = value;
+        console.log(result);
+        this.temp_for_demo = result;
       }
     });
   }
 
+  buildFormFromItems(){
+
+  }
+
   selectQuestionerSection(item: any) {
     this.temp_for_demo['item'] = this.temp_for_demo.item.map((element: any) => element == item ? {...element, selected: true}: {...element, selected: false});
+  }
+
+  onSubmit() {
+    console.log(this.temp_for_demo)
   }
 }
