11a1852c7af39719a02c186f8e80946088501040
Plamen Tassev <plamen@gatech.edu>
2024-04-12 17:06:34 -0400

    A very simple form model

--- src/app/components/form-viewer/form-viewer.component.ts
+++ src/app/components/form-viewer/form-viewer.component.ts
@@ -1,3 +1,9 @@
+export enum QuestionWidgetType{
+  CHECKBOX = 'choice',
+  INPUT = 'string'
+}
+
+
 import {Component, OnInit} from '@angular/core';
 import {RcApiInterfaceService} from "../../services/rc-api-interface/rc-api-interface.service";
 
@@ -9,12 +15,23 @@ import {RcApiInterfaceService} from "../../services/rc-api-interface/rc-api-inte
 export class FormViewerComponent implements OnInit {
 
   temp_for_demo: any;
+  QuestionWidgetType = QuestionWidgetType;
   constructor(private rcApiInterfaceService: RcApiInterfaceService) {}
 
   ngOnInit(): void {
     // TODO: Change this to not be static, once linked to the input should be based on the FormSummary name (NOT title)
     this.rcApiInterfaceService.getJobPackage("SETNETInfantFollowUp").subscribe({
-      next: value => {console.log(value); this.temp_for_demo = value}
-    })
-    }
+      next: value => {
+        console.log(value);
+        value['item'] = value['item']?.map((item: any, index: number) => {
+          return index == 0 ? {...item, selected: true} : {...item, selected: false}
+        });
+        this.temp_for_demo = value;
+      }
+    });
+  }
+
+  selectQuestionerSection(item: any) {
+    this.temp_for_demo['item'] = this.temp_for_demo.item.map((element: any) => element == item ? {...element, selected: true}: {...element, selected: false});
+  }
 }
