c22f40cbee86fd8bc9bd97488368a1f27472b19d
Plamen Tassev <plamen@gatech.edu>
2024-04-25 09:01:24 -0400

    Cleaned some of the styles prior to demo

--- src/app/directives/has-evidence.directive.ts
+++ src/app/directives/has-evidence.directive.ts
@@ -5,12 +5,12 @@ import {Directive, ElementRef, Input, OnInit} from '@angular/core';
   standalone: true
 })
 export class HasEvidenceDirective implements OnInit{
-  @Input() extensionList: any;
+  @Input() extensionList: any; //TODO replace type any with a specific type
   constructor(private el: ElementRef) { }
 
   ngOnInit(): void {
     if (this.extensionList?.some( ext =>
-      (ext.url === "http://gtri.gatech.edu/fakeFormIg/cqlTask")
+      (ext.url === "http://gtri.gatech.edu/fakeFormIg/cqlTask") //TODO we need to extract the url strings to constants to constants.
         ||
       (ext.url === "http://gtri.gatech.edu/fakeFormIg/nlpqlTask"))) {
       this.el.nativeElement.style.display = 'block';
