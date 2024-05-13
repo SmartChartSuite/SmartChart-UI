81ff8a5bf282468788fdcd918139cce636b21a16
Plamen Tassev <plamen@gatech.edu>
2024-05-13 15:22:54 -0400

    Unstructured evidence created cards

--- src/app/components/form-viewer/evidence-details/unstructured-results-details/unstructured-results-details.component.ts
+++ src/app/components/form-viewer/evidence-details/unstructured-results-details/unstructured-results-details.component.ts
@@ -2,7 +2,6 @@ import {Component, Input} from '@angular/core';
 import {openDocumentViewerModal} from "../../document-viewer-modal/document-viewer-modal.component";
 import {MatDialog} from "@angular/material/dialog";
 import {NlpAnswer} from "../../../../models/results";
-import {FhirBaseResource} from "../../../../models/fhir/fhir.base.resource";
 
 @Component({
   selector: 'app-unstructured-results-details',
@@ -10,8 +9,7 @@ import {FhirBaseResource} from "../../../../models/fhir/fhir.base.resource";
   styleUrl: './unstructured-results-details.component.scss'
 })
 export class UnstructuredResultsDetailsComponent {
-  @Input() nlpResources: FhirBaseResource[] = [];
-  @Input() nlpAnswers: NlpAnswer[] = [];
+  @Input() nlpAnswer: NlpAnswer;
   constructor(private dialog: MatDialog){}
 
   onExpandDocument() {
@@ -19,7 +17,7 @@ export class UnstructuredResultsDetailsComponent {
       this.dialog,
       {
         title: "Document Content",
-        content: "Sample Content",
+        content: this.nlpAnswer.fullText,
         size: {
           minWidth: "500px",
           minHeight: "300px"
