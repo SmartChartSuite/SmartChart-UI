e0005db436d180cb007dcfbd0167dc9eac2588c9
Ellie Shivers <elliemshivers@gmail.com>
2024-06-26 13:53:07 -0400

    Fixes to authorization handling of audience

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {JwksValidationHandler, OAuthService} from "angular-oauth2-oidc";
 import {authCodeFlowConfig} from "../../../assets/config/auth-code-flow-config";
+import {ConfigService} from "../../services/config/config.service";
 @Component({
   selector: 'sc-standalone-login',
   templateUrl: './login.component.html',
@@ -8,13 +9,15 @@ import {authCodeFlowConfig} from "../../../assets/config/auth-code-flow-config";
 })
 export class LoginComponent {
   @Input({ required: true }) isLocatedInMainMenu!: boolean;
-  constructor(public oauthService: OAuthService) {
+  constructor(public oauthService: OAuthService, public configService: ConfigService,
+  ) {
     this.configure();
   }
 
   private configure() {
     // Load information from Auth0 (could also be configured manually)
     this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.customQueryParams = this.configService.config.auth.customQueryParams;
     this.oauthService.tokenValidationHandler = new JwksValidationHandler();
     this.oauthService.loadDiscoveryDocumentAndTryLogin();
   }
