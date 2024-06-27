ea6d78811d66590b5dcfcc4ccd8442a13a2595e2
Plamen Tassev <plamen@gatech.edu>
2024-06-27 16:39:22 -0400

    Implemented structured evidence pagination

--- src/app/components/form-viewer/evidence-details/structured-results-details/structured-results-details.component.ts
+++ src/app/components/form-viewer/evidence-details/structured-results-details/structured-results-details.component.ts
@@ -1,17 +1,19 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {MatTableDataSource} from "@angular/material/table";
 import {StructuredEvidenceDTO} from "../../../../models/structured-evidence-dto/structured-evidence-dto";
 import {EvidenceViewerService} from "../../../../services/evidence-viewer/evidence-viewer.service";
 import {Observable} from "rxjs";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-structured-results-details',
   templateUrl: './structured-results-details.component.html',
   styleUrl: './structured-results-details.component.scss'
 })
-export class StructuredResultsDetailsComponent implements OnChanges, OnInit{
+export class StructuredResultsDetailsComponent implements OnChanges, OnInit, AfterViewInit{
   @Input() structuredEvidenceDto: StructuredEvidenceDTO[] = [];
   @Input() displayedColumns: string[] = []; // Allows the user to enter the table columns of their choice in the order they need
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   columns: string[];
   dataSource = new MatTableDataSource<StructuredEvidenceDTO>([]);
@@ -20,10 +22,14 @@ export class StructuredResultsDetailsComponent implements OnChanges, OnInit{
 
   constructor(private evidenceViewerService: EvidenceViewerService) {
   }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if(this.structuredEvidenceDto){
       this.dataSource.data = this.structuredEvidenceDto;
+      this.dataSource.paginator = this.paginator;
       if(this.displayedColumns?.length > 0){
         this.columns = this.displayedColumns;
       }
