5b1d9fc593a9d82c5d129f4bcdcc8aacbe6d8341
Plamen Tassev <plamen@gatech.edu>
2024-07-01 16:22:52 -0400

    pushing broken encouter code

--- src/app/models/structured-evidence-dto/encounter-dto.ts
+++ src/app/models/structured-evidence-dto/encounter-dto.ts
@@ -4,25 +4,25 @@ import {System} from "./system";
 import {PatientSummary} from "../patient-summary";
 
 export class EncounterDTO extends StructuredEvidenceDTO {
-  periodStart: string; //TODO: verify the criteria with the rest of the team.
-  periodEnd: string;
-  dateAgeAt: string;
+  startDateAgeAt: string;
   encounterType: string;
   reasonCode: string;
   reasonSystem: string;
   reasonConceptName: string;
+  endDateAgeAt: string;
 
   constructor(encounter: FhirBaseResource, patientSummary: PatientSummary){
     super();
     console.log(encounter); //TODO: Remove console.log
-    this.periodStart = encounter["period"]?.["start"];
-    this.dateAgeAt = super.getDateAgeAsStr(this.periodStart, patientSummary.birthDate); //TODO: verify requirements
-    this.periodEnd = encounter["period"]?.["end"];
+    const periodStart = encounter["period"]?.["start"];
+    this.startDateAgeAt = super.getDateAgeAsStr(periodStart, patientSummary.birthDate); //TODO: verify requirements
+    const periodEnd = encounter["period"]?.["end"];
+    this.endDateAgeAt = super.getDateAgeAsStr(periodEnd, patientSummary.birthDate); //TODO: verify requirements
     this.encounterType = encounter["type"]?.[0]?.["text"] || encounter["type"]?.[0]?.["coding"] ?.[0]?.display
     const reasonCode = super.getCode(encounter, 'reasonCode', [System.ICD_10, System.SNOMED]);
     this.reasonCode = reasonCode?.code;
-    this.reasonSystem =  super.getSystemFromEnum(reasonCode?.system)
-    this.reasonConceptName = reasonCode?.text || reasonCode[0]?.coding?.display;
+    this.reasonSystem =  super.getSystemFromEnum(reasonCode?.system);
+    this.reasonConceptName = reasonCode?.text || reasonCode?.display;
   }
 
   public static sort(a, b) {
