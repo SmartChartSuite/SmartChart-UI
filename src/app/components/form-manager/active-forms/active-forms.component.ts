464df9d408e55bc05e732e5126a7dae202d2de6f
Plamen Tassev <plamen@gatech.edu>
2024-08-19 13:04:48 -0600

    temp committ

--- src/app/components/form-manager/active-forms/active-forms.component.ts
+++ src/app/components/form-manager/active-forms/active-forms.component.ts
@@ -5,7 +5,9 @@ import {MatTableDataSource} from "@angular/material/table";
 import {Router} from "@angular/router";
 import {FormManagerService} from "../../../services/form-manager/form-manager.service";
 import {UtilsService} from "../../../services/utils/utils.service";
-
+import {FormControl, FormGroup} from "@angular/forms";
+export const Gender = ["Male", "Female", "Other", "Unknown"];
+export const Status = ["any", "active"]
 @Component({
   selector: 'app-active-forms',
   templateUrl: './active-forms.component.html',
@@ -13,9 +15,32 @@ import {UtilsService} from "../../../services/utils/utils.service";
 })
 export class ActiveFormsComponent implements OnInit {
 
-  displayedColumns: string[] = ["name", "gender", 'dob', "formName", "started" ];
+  displayedColumns: string[] = ["name", "gender", "dob", "formName", "started" ];
   dataSource: MatTableDataSource<ActiveFormSummary> = new MatTableDataSource<ActiveFormSummary>([]);
   isLoading = false;
+
+  readonly GENDER_LIST = Gender;
+  readonly STATUS_LIST = Status;
+  formNameList : string[] = [];
+
+  dobRange = new FormGroup({
+    start: new FormControl(null),
+    end: new FormControl(null),
+  });
+
+  startedRange = new FormGroup({
+    start: new FormControl(null),
+    end: new FormControl(null),
+  });
+
+  searchResultsForm = new FormGroup({
+    patientName: new FormControl(''),
+    gender: new FormControl(this.GENDER_LIST[0]),
+    dobRange: new FormControl('', this.dobRange),
+    name: new FormControl(''),
+    startedRange: new FormControl('', this.startedRange),
+    status: new FormControl(this.GENDER_LIST[0]),
+  });
 
   constructor(
     private rcApiInterfaceService: RcApiInterfaceService,
@@ -23,6 +48,21 @@ export class ActiveFormsComponent implements OnInit {
     private router: Router,
     private utilsService: UtilsService
   ) {}
+
+  getFormList(){
+    this.isLoading = true;
+    this.rcApiInterfaceService.getSmartChartUiQuestionnaires().subscribe({
+      next: value => {
+       // this.formNameList = value.unshift('Any');
+        this.isLoading = false;
+      },
+      error: err => {
+        this.isLoading = false;
+        this.utilsService.showErrorMessage();
+        console.error(err);
+      }
+    });
+  }
   ngOnInit(): void {
     this.getBatchJobs();
 
@@ -50,5 +90,9 @@ export class ActiveFormsComponent implements OnInit {
   onActiveFormSelected(activeFormSummary: ActiveFormSummary) {
     this.formManagerService.setSelectedActiveFormSummary(activeFormSummary);
     this.router.navigate([`/form-viewer`]);
+  }
+
+  onSubmit() {
+
   }
 }
