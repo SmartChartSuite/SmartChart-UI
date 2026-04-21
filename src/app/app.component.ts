2296f26cc0ff509e166355c7c5a28ce6515a8fc6
ptassev3 <plamentassev@gmail.com>
2026-04-21 16:01:49 -0400

    added a header and logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,13 @@ import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import {HeaderComponent} from "./components/header/header.component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [SideNavComponent, LoadingComponent, RouterOutlet, LoginComponent]
+  imports: [SideNavComponent, LoadingComponent, RouterOutlet, LoginComponent, HeaderComponent]
 })
 export class AppComponent {
   title = '';
