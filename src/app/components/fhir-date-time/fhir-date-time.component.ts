f6d13fe46862d9fb6a457e0984fca31292853e99
ptassev3 <plamentassev@gmail.com>
2026-07-24 14:51:38 -0400

    updated packages

--- src/app/components/fhir-date-time/fhir-date-time.component.ts
+++ src/app/components/fhir-date-time/fhir-date-time.component.ts
@@ -1,10 +1,21 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ChangeDetectionStrategy} from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ChangeDetectionStrategy,
+  OnDestroy
+} from '@angular/core';
 import { FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import {QuestionnaireItemType} from "../../models/fhir/valuesets/questionnaire-item-type";
 import { MatFormField, MatLabel, MatHint, MatSuffix } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatDatepickerInput, MatDatepickerToggle, MatDatepicker } from '@angular/material/datepicker';
 import {MatTimepickerInput, MatTimepickerModule} from "@angular/material/timepicker";
+import {Subject, takeUntil} from "rxjs";
 
 const timeRegex = /([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]{1,9})?/;
 const dateRegex =  /([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1]))?)?/;
@@ -30,17 +41,19 @@ const dateTimeRegex =  /([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1
     MatTimepickerModule
   ]
 })
-export class FhirDateTimeComponent implements OnChanges, OnInit {
+export class FhirDateTimeComponent implements OnChanges, OnInit, OnDestroy {
 
   @Input() inputValue: string;
   @Input() questionType: QuestionnaireItemType;
   @Output() onDateTimeUpdated = new EventEmitter<any>();
 
   // protected readonly TIMEZONES = TIMEZONES;
+  private destroy$ = new Subject<void>();
   protected readonly QuestionnaireItemType = QuestionnaireItemType;
   private readonly timeRegex = timeRegex;
   private readonly dateRegex = dateRegex;
   private readonly dateTimeRegex = dateTimeRegex;
+  private isInternalUpdate = false; // Flag to track internal updates
 
 
   form = new FormGroup({
@@ -49,125 +62,152 @@ export class FhirDateTimeComponent implements OnChanges, OnInit {
   });
 
   ngOnChanges(changes: SimpleChanges): void {
-    // Set validators when questionType changes
     if (changes['questionType']?.currentValue) {
-      const questionType = changes['questionType'].currentValue;
-
-      if (questionType === QuestionnaireItemType.time) {
-        this.form.controls['time'].setValidators(Validators.required);
-        this.form.controls['time'].updateValueAndValidity();
-      } else if (questionType === QuestionnaireItemType.date) {
-        this.form.controls['date'].setValidators(Validators.required);
-        this.form.controls['date'].updateValueAndValidity();
-      } else if (questionType === QuestionnaireItemType.dateTime) {
-        this.form.controls['date'].setValidators(Validators.required);
-        this.form.controls['time'].setValidators(Validators.required);
-        this.form.controls['date'].updateValueAndValidity();
-        this.form.controls['time'].updateValueAndValidity();
-      }
+      this.updateValidators(changes['questionType'].currentValue);
     }
 
-    if (changes['inputValue']?.currentValue ){
-      // Check if the input value is in correct fhir format
-      this.inputValue = this.inputValue.replaceAll(' ','');
-      if(!this.checkValidInput(this.inputValue, this.questionType)){
-        console.error(`Invalid ${this.questionType} detected with value ${this.inputValue}`);
-        return;
-      }
+    // Skip processing if this is an internal update (from our own emission)
+    if (this.isInternalUpdate) {
+      this.isInternalUpdate = false;
+      return;
+    }
 
-      if(this.questionType == QuestionnaireItemType.date){
-        const date = this.getDateFromISOString(this.inputValue, 'date');
-        this.form.controls['date'].setValue(date, { emitEvent: false });
-      }
-      else if (this.questionType == QuestionnaireItemType.dateTime){
-        const date = this.getDateFromISOString(this.inputValue, 'date');
-        this.form.controls['date'].setValue(date, { emitEvent: false });
-
-        const time = this.getDateFromISOString(this.inputValue, 'time')
-        this.form.controls['time'].setValue(time, { emitEvent: false });
-      }
-      else if(this.questionType == QuestionnaireItemType.time){
-        //The widget accepts data in format HH:MM only. If the format is HH:MM:SS, we need to truncate the SS part
-        const time = this.inputValue.length > 5 ? this.inputValue.substring(0, 5) : this.inputValue;
-        this.form.controls['time'].setValue(time, { emitEvent: false });
-      }
+    // Only process valid inputValue changes
+    if (changes['inputValue'] && this.inputValue?.trim()) {
+      this.processInputValue();
     }
   }
 
-  getDateFromISOString(utcString, dataType: 'date' | 'time') {
-    if (!utcString || utcString.length == 0) {
-      return null;
+  private updateValidators(questionType: QuestionnaireItemType): void {
+    const dateControl = this.form.controls['date'];
+    const timeControl = this.form.controls['time'];
+
+    // Clear existing validators
+    dateControl.clearValidators();
+    timeControl.clearValidators();
+
+    // Set validators based on question type
+    if (questionType === QuestionnaireItemType.time) {
+      timeControl.setValidators(Validators.required);
+    } else if (questionType === QuestionnaireItemType.date) {
+      dateControl.setValidators(Validators.required);
+    } else if (questionType === QuestionnaireItemType.dateTime) {
+      dateControl.setValidators(Validators.required);
+      timeControl.setValidators(Validators.required);
     }
+
+    dateControl.updateValueAndValidity();
+    timeControl.updateValueAndValidity();
+  }
+
+  private processInputValue(): void {
+    this.inputValue = this.inputValue.replaceAll(' ', '');
+
+    if (!this.checkValidInput(this.inputValue, this.questionType)) {
+      console.warn(`Invalid ${this.questionType} detected with value ${this.inputValue}`);
+      return;
+    }
+
+    // Skip if value hasn't changed
+    const currentFormValue = this.formValueToStr(this.form.value, this.questionType);
+    if (currentFormValue === this.inputValue) {
+      return;
+    }
+
+    // Update form based on question type
+    if (this.questionType === QuestionnaireItemType.date) {
+      const date = this.getDateFromISOString(this.inputValue, 'date');
+      this.form.patchValue({ date }, { emitEvent: false });
+    } else if (this.questionType === QuestionnaireItemType.dateTime) {
+      const date = this.getDateFromISOString(this.inputValue, 'date');
+      const time = this.getDateFromISOString(this.inputValue, 'time');
+      this.form.patchValue({ date, time }, { emitEvent: false });
+    } else if (this.questionType === QuestionnaireItemType.time) {
+      // Widget accepts HH:MM format only
+      const time = this.inputValue.length > 5 ? this.inputValue.substring(0, 5) : this.inputValue;
+      this.form.patchValue({ time }, { emitEvent: false });
+    }
+  }
+
+  private getDateFromISOString(utcString: string, dataType: 'date' | 'time'): string | null {
+    if (!utcString) return null;
+
     const dateObj = new Date(utcString);
-    if (dataType == 'date') {
+
+    if (dataType === 'date') {
       const year = dateObj.getUTCFullYear();
       const month = (dateObj.getUTCMonth() + 1).toString().padStart(2, '0');
       const day = dateObj.getUTCDate().toString().padStart(2, '0');
-      const date = `${year}-${month}-${day}`;
-      return date;
+      return `${year}-${month}-${day}`;
     }
 
-    // Extract time components
-    else if (dataType) {
+    if (dataType === 'time') {
       const hours = dateObj.getUTCHours().toString().padStart(2, '0');
       const minutes = dateObj.getUTCMinutes().toString().padStart(2, '0');
-      const time = `${hours}:${minutes}`;
-      return time;
-    } else return null;
+      return `${hours}:${minutes}`;
+    }
 
+    return null;
   }
 
-  // private getTimezone(inputValue?: string) {
-  //   if(!inputValue){
-  //     this.TIMEZONES.find(zone => zone.value == "+00:00");
-  //   }
-  //   //TODO implement get timezone based on input string value
-  //   return null;
-  // }
-
   ngOnInit(): void {
-    this.form.valueChanges.subscribe(value => {
+    this.form.valueChanges.pipe(takeUntil(this.destroy$)).subscribe(value => {
       const strValue = this.formValueToStr(value, this.questionType);
-      this.onDateTimeUpdated.emit({value: strValue, questionType: this.questionType})
+      // Only emit if we have a valid value to prevent clearing the form
+      if (strValue) {
+        this.isInternalUpdate = true; // Mark as internal update before emitting
+        this.onDateTimeUpdated.emit({value: strValue, questionType: this.questionType})
+      }
     })
   }
 
-  private formValueToStr(value: any, questionType: QuestionnaireItemType) {
-    let result = '';
-    if(questionType == QuestionnaireItemType.date){
-      const date = new Date(value.date);
-      result =  date.toISOString().split('T')[0];
+  private formValueToStr(value: any, questionType: QuestionnaireItemType): string {
+    if (questionType === QuestionnaireItemType.date) {
+      return this.formatDate(value.date);
     }
-    else if(questionType == QuestionnaireItemType.time){
-      result = `${value.time}:00`
+
+    if (questionType === QuestionnaireItemType.time) {
+      return value.time ? `${value.time}:00` : '';
     }
-    else if(questionType == QuestionnaireItemType.dateTime){
-      //TODO verify the assumption that the user is required to enter a date and time (and not only date), and change the if statement accordingly
-      if(this.form.controls['date'].value && this.form.controls['time'].value){
-        const date = this.form.controls['date'].value;
-        // We need to remove the time and from the ISO string, we add the time selected by the user with the time widget.
-        const utcDateStr = new Date(date).toISOString().split('T')[0];
-        // We append the time here. This assumes that the user is required to add both: time and date
-        const time = `${this.form.controls['time'].value}:00.000Z`;
-        result =  `${utcDateStr}T${time}`
-      }
+
+    if (questionType === QuestionnaireItemType.dateTime) {
+      return this.formatDateTime(value.date, value.time);
     }
-    return result;
+
+    return '';
+  }
+
+  private formatDate(dateValue: any): string {
+    if (!dateValue) return '';
+
+    const date = new Date(dateValue);
+    if (isNaN(date.getTime())) return '';
+
+    return date.toISOString().split('T')[0];
+  }
+
+  private formatDateTime(dateValue: any, timeValue: string): string {
+    if (!dateValue || !timeValue) return '';
+
+    const dateObj = typeof dateValue === 'string' ? new Date(dateValue) : dateValue;
+    if (!(dateObj instanceof Date) || isNaN(dateObj.getTime())) return '';
+
+    const datePart = dateObj.toISOString().split('T')[0];
+    return `${datePart}T${timeValue}:00.000Z`;
   }
 
   private checkValidInput(inputValue: string, questionType: QuestionnaireItemType): boolean {
-    if(questionType == QuestionnaireItemType.time){
-      const regExp = new RegExp(this.timeRegex);
-      return regExp.test(inputValue);
-    }
-    if(questionType == QuestionnaireItemType.date){
-      const regExp = new RegExp(this.dateRegex);
-      return regExp.test(inputValue);
-    }
-    if(questionType == QuestionnaireItemType.dateTime){
-      const regExp = new RegExp(this.dateTimeRegex);
-      return regExp.test(inputValue);
-    }
-    else return false;
+    const regexMap = {
+      [QuestionnaireItemType.time]: this.timeRegex,
+      [QuestionnaireItemType.date]: this.dateRegex,
+      [QuestionnaireItemType.dateTime]: this.dateTimeRegex
+    };
+
+    const regex = regexMap[questionType];
+    return regex ? new RegExp(regex).test(inputValue) : false;
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
