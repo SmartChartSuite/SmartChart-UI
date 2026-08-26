83e52b925cfd40eea6882a40888982cc6834ff56
ptassev3 <plamentassev@gmail.com>
2026-08-26 15:36:51 -0400

    Fixed: remove dead code for create groups dialogue

--- src/app/components/patient-groups/patient-groups.component.ts
+++ src/app/components/patient-groups/patient-groups.component.ts
@@ -5,10 +5,7 @@ import {RcApiInterfaceService} from "../../services/rc-api-interface/rc-api-inte
 import { MatSelectChange, MatSelect, MatOption } from "@angular/material/select";
 import {FormManagerService} from "../../services/form-manager/form-manager.service";
 import {UtilsService} from "../../services/utils/utils.service";
-import {openCreateGroupDialog} from "../create-group-dialog/create-group-dialog.component";
-import {MatDialog} from "@angular/material/dialog";
 import { MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatButton } from '@angular/material/button';
 import { PatientSummaryTableComponent } from '../patient-summary-table/patient-summary-table.component';
 
 @Component({
@@ -28,7 +25,6 @@ export class PatientGroupsComponent implements OnInit{
     private rcApiInterfaceService: RcApiInterfaceService,
     private formManagerService: FormManagerService,
     private utilsService: UtilsService,
-    private dialog: MatDialog,
   ){}
 
 
@@ -48,19 +44,6 @@ export class PatientGroupsComponent implements OnInit{
       }
     });
   }
-
-  onCreateNewGroup() {
-    openCreateGroupDialog(
-      this.dialog,
-      {})
-      .subscribe(
-        result => {
-            //TODO finish implementation
-          console.log(result);
-        }
-      )
-  }
-
 
   onGroupSelected(event: MatSelectChange<PatientGroup>) {
     this.selectedGroup.set(event.value);
